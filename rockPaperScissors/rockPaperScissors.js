3b8bbf9017a73186caf77bd86ca95cd30b6e9a9b
savybrandt <savybrandt@gmail.com>
2016-12-20 10:43:52 -0800

    still debugging, tried commiting earlier but didn't go through

--- rockPaperScissors/rockPaperScissors.js
+++ rockPaperScissors/rockPaperScissors.js
@@ -17,7 +17,44 @@
 *
 */
 
-var rockPaperScissors = function (
-) {
-  // TODO: your solution here
+var rockPaperScissors = function (rounds) {
+	var throws = ['rock', 'paper', 'scissors'];
+	var results = [];
+	var resultCount = 0;
+
+	var roundGenerator = function() {
+	  var round = [];
+		//create random possible outcome for a 3 round game
+		while (rounds > 0) {
+			var randomThrow = throws[Math.floor(Math.random() * throws.length)];
+			round.push(randomThrow);
+			rounds--;
+		}
+		//check if that round already exists in our results array
+		var flag = true;
+
+		for (var i = 0; i < results.length; i++) {
+		  var resultCheck = results[i];
+			for (var j = 0; j < resultCheck.length; i++) {
+				if (resultCheck[j] === round[j]) {
+					flag = false;
+				}
+			}
+			//dubug- ***flag at this point is currently coming up as undefined when it should be true or false***
+
+			//if it doesnt, add it to our array if it does rerun roundGenerator
+			if (flag) {
+				results.push(round);
+				resultCount++
+			} else {
+			  roundGenerator()
+			}
+		}
+	};
+	//check if we've gotten all results, if yes return, if not run roundGenerator
+	if (resultCount === 27) {
+		return results;
+	} else {
+		roundGenerator()
+	}
 };
