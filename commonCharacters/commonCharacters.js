7e08da14e3aba2c1eb10cb0399721028acf8156d
savybrandt <savybrandt@gmail.com>
2017-02-21 09:28:57 -0800

    naive solution

--- commonCharacters/commonCharacters.js
+++ commonCharacters/commonCharacters.js
@@ -13,5 +13,11 @@
 
 
 var commonCharacters = function(string1, string2) {
-  // TODO: Your code here!
+  var results = []
+  for (var i = 0; i < string1.length; i++) {
+  	if(string2.indexOf(string1[i]) !== -1) {
+  		results.push(string1[i])
+  	}
+  }
+  return results.join('')
 };
